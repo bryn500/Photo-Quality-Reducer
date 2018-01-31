8837cbf6604c60672c55ced895c995dcdcf1e6a4
Bryn Thomas <bryn500@outlook.com>
2018-01-31 16:00:53 +0000

    Initial commit

---
+++ Photo Quality Reducer/Program.cs
@@ -0,0 +1,41 @@
+﻿using ImageProcessor;
+using ImageProcessor.Imaging;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Photo_Quality_Reducer
+{
+    class Program
+    {
+        const string PhotoSource = "c://Photos";
+        const string PhotoDestination = "c://Photos-Optimised";
+
+        static void Main(string[] args)
+        {
+            // Create Destination folder if it doesn't exist
+            Directory.CreateDirectory(PhotoDestination);
+
+            using (ImageFactory imageFactory = new ImageFactory(preserveExifData: true))
+            {
+                var resizelayer = new ResizeLayer(new System.Drawing.Size(1500, 1500), ResizeMode.Min);
+
+                string[] files = Directory.GetFiles(PhotoSource);
+
+                int i = 1;
+
+                foreach (var path in files)
+                {
+                    var filename = Path.GetFileName(path);
+
+                    imageFactory.Load(Path.Combine(PhotoSource, filename))
+                        .Resize(resizelayer)
+                        .Quality(90)
+                        .Save(Path.Combine(PhotoDestination, filename));
+
+                    Console.WriteLine(i++);
+                }
+            }
+        }
+    }
+}
